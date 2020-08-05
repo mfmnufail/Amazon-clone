bd713ab0abc10864fb5942cb5a07f253b7045e99
Mohamed Nufail <media3254@gmail.com>
2020-08-06 00:50:37 +0530

    Nav bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from './Header';
+
 
 function App() {
   return (
-    <div className="App">
-     <h1>Is this working</h1>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+        
+          <Route path = "/login"><h1>Login page</h1></Route>
+          <Route path = "/checkout">Checkout page</Route>
+          <Route path = "/">
+            <Header/>
+            Home
+          </Route>
+        </Switch>
+      </div>
+
+    </Router>
+    
   );
 }
 
