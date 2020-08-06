e4cc158fadf20c58d842633f00b6c365f8390f20
Mohamed Nufail <media3254@gmail.com>
2020-08-06 20:26:55 +0530

    Banner

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from './Header';
+import Home from "./Home"
 
 
 function App() {
@@ -10,10 +11,16 @@ function App() {
       <div className="app">
         <Switch>
         
-          <Route path = "/login"><h1>Login page</h1></Route>
-          <Route path = "/checkout">Checkout page</Route>
+          <Route path = "/login">
+            <h1>Login page</h1>
+            </Route>
+          <Route path = "/checkout">
+          <Header/>
+            Checkout page
+            </Route>
           <Route path = "/">
             <Header/>
+            <Home/>
             Home
           </Route>
         </Switch>
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Home.css"
+
+function Home() {
+    return (
+        <div className="home">
+            <img className = "home__image" src="https://images-na.ssl-images-amazon.com/images/G/15/digital/video/merch/2020/TV/BRND/BRND_MTH20_00000_GWBleedingHero_1500x600_Final_en-CA_ENG_PVD5220._CB408289997_.jpg" alt=""/>
+            <h1>I am in the home page</h1>
+        </div>
+    )
+}
+
+export default Home
